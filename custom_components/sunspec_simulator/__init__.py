08465d10b68009033783a8168eb52d09e125f7ce
yinchak <57385082+yinchak@users.noreply.github.com>
2025-03-25 17:14:34 +0800

    Update __init__.py

--- custom_components/sunspec_simulator/__init__.py
+++ custom_components/sunspec_simulator/__init__.py
@@ -1,14 +1,16 @@
 """SunSpec Simulator Custom Component."""
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.discovery import async_load_platform
 
 DOMAIN = "sunspec_simulator"
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
-    """Set up the SunSpec Simulator component."""
+    """Set up SunSpec Simulator."""
+    hass.data[DOMAIN] = {}
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up SunSpec Simulator from a config entry."""
-    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "sensor"))
+    hass.async_create_task(async_load_platform(hass, "sensor", DOMAIN, {}, entry))
     return True
