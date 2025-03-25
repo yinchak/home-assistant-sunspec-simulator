1636f8ea8d9970a02cafb2766676ea16f62485fd
yinchak <57385082+yinchak@users.noreply.github.com>
2025-03-25 16:15:29 +0800

    Update __init__.py

--- custom_components/sunspec_simulator/__init__.py
+++ custom_components/sunspec_simulator/__init__.py
@@ -5,20 +5,7 @@ DOMAIN = "sunspec_simulator"
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     """Set up the SunSpec Simulator component."""
-    return True
-
-async def async_setup_entry(hass: HomeAssistant, entry):
-    """Set up SunSpec Simulator from a config entry."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "binary_sensor")
-    )
-    return True
-
-async def async_unload_entry(hass: HomeAssistant, entry):
-    """Unload a config entry."""
-    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    await hass.config_entries.async_forward_entry_unload(entry, "binary_sensor")
+    # 喺呢度直接設實體
+    from .sensor import async_setup_sensors
+    await async_setup_sensors(hass, config)
     return True
